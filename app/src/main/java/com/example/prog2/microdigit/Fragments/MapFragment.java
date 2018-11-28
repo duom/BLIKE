84c2d23cf09f2b37695f969754819ab52b1b8da7
Albert Fernandez <albertfernandezgarcia@gmail.com>
2018-11-28 19:07:22 +0100

    28/11/18 App funciona perfecto + ubicacion y primeras rutas

--- app/src/main/java/com/example/prog2/microdigit/Fragments/MapFragment.java
+++ app/src/main/java/com/example/prog2/microdigit/Fragments/MapFragment.java
@@ -1,9 +1,15 @@
 package com.example.prog2.microdigit.Fragments;
 
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,16 +34,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private View rootView;
     private GoogleMap mMap;
     private MapView mapView;
-    //**op 3**//
-    //GoogleMap gMap;
-    ///***///
 
-
-    //******opcion2******//
-//    private Marker marcador;
-//    double lat = 0.0;
-//    double lng = 0.0;
-    /////********///
+    GoogleMap gMap;
 
 
     public MapFragment() {
@@ -74,35 +72,30 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
 
         //////////*********opcion 3 alberto********////////////
-        //gMap = googleMap;
+        gMap = googleMap;
 
-//        if (ActivityCompat.checkSelfPermission(com.example.prog2.microdigit.Fragments.MapFragment.super.getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(MapsActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-//        }else{
-//            if(!gMap.isMyLocationEnabled())
-//                gMap.setMyLocationEnabled(true);
-//
-//            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-//            Location myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//
-//            if (myLocation == null) {
-//                Criteria criteria = new Criteria();
-//                criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-//                String provider = lm.getBestProvider(criteria, true);
-//                myLocation = lm.getLastKnownLocation(provider);
-//            }
-//
-//            if(myLocation!=null){
-//                LatLng userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-//                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 14), 1500, null);
-//
-//            }
-//        }
-        ///////***********************//////////
+        if (ActivityCompat.checkSelfPermission(MapFragment.this.getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapFragment.this.getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapFragment.this.getActivity(),new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }else{
+            if(!gMap.isMyLocationEnabled())
+                gMap.setMyLocationEnabled(true);
 
-        //***opcion 2***//
-        //miUbicacion();
-          ///****///
+            LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+            Location myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (myLocation == null) {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+                String provider = lm.getBestProvider(criteria, true);
+                myLocation = lm.getLastKnownLocation(provider);
+            }
+
+            if(myLocation!=null){
+                LatLng userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 14), 1500, null);
+
+            }
+       }
 
         //1 full mapa
         //5 nivel continente
@@ -115,23 +108,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mMap.setMinZoomPreference(10);
         mMap.setMaxZoomPreference(21);
 
-        // *****Opcion 1 para mostrar ubicación (solo este codigo)*****/////
-//        if (ActivityCompat.checkSelfPermission(com.example.prog2.microdigit.Fragments.MapFragment.super.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-//                && ActivityCompat.checkSelfPermission(com.example.prog2.microdigit.Fragments.MapFragment.super.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            return;
-//        }
-//        mMap.setMyLocationEnabled(true);
-        ///////************////////
-
-        // Añadir marcador
-
         //LatLng hayma = new LatLng(41.452237940530296, 2.209321909764924);
         LatLng sanJeronimo = new LatLng(41.490057, 2.223802);
         LatLng flow = new LatLng(41.487486, 2.227633);
         LatLng calDimoni = new LatLng(41.486315, 2.227802);
         LatLng mataMachos = new LatLng(41.488667, 2.227708);
         LatLng regueros = new LatLng(41.492123, 2.226508);
-
 
         //añadios marcador y le decimos que sea draggable(true)
 
@@ -171,10 +153,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 //        mMap.addMarker(new MarkerOptions().position(hayma).title("4 Pussy").draggable(true));
 //        mMap.addMarker(new MarkerOptions().position(hayma).title("Castillo").draggable(true));
 //
-
-
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Esto es sidney bro").draggable(true));
-
 
         //Configurar la posicion por defecto de la camara al iniciar.
 
@@ -188,7 +167,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camera));
 
-
         //Hacer click al mapa e interacturar (te devuelve latitud y long)
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -199,7 +177,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                         "Lon: " + latLng.longitude, Toast.LENGTH_SHORT).show();
             }
         });
-
         // Pulsar manteniendo
 
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
@@ -209,10 +186,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 Toast.makeText(rootView.getContext(), "Largo Click aqui: \n" +
                         "Lat: " + latLng.latitude + "\n" +
                         "Lon: " + latLng.longitude, Toast.LENGTH_SHORT).show();
-
             }
         });
-        //
         //arrastar y dejar en el mapa
 
         mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
@@ -237,63 +212,5 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         });
 
     }
-    //// ***********Opcion 2 mostrar ubicacion me da probleas al final por el context..getSystemService********/////
-//    private void agregarMarcador(double lat, double lng) {
-//        LatLng coordenadas = new LatLng(lat, lng);
-//        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
-//        if (marcador != null) marcador.remove();
-//        marcador = mMap.addMarker(new MarkerOptions()
-//                .position(coordenadas)
-//                .title("Mi posicion actual")
-//                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
-//        mMap.animateCamera(miUbicacion);
-//    }
-//
-//    private void actualizarUbicacion(Location location) {
-//        if (location != null) {
-//            lat = location.getLatitude();
-//            lng = location.getLongitude();
-//            agregarMarcador(lat, lng);
-//        }
-//    }
-//
-//    LocationListener locListener = new LocationListener() {
-//        @Override
-//        public void onLocationChanged(Location location) {
-//            actualizarUbicacion(location);
-//        }
-//
-//        @Override
-//        public void onStatusChanged(String provider, int status, Bundle extras) {
-//
-//        }
-//
-//        @Override
-//        public void onProviderEnabled(String provider) {
-//
-//        }
-//
-//        @Override
-//        public void onProviderDisabled(String provider) {
-//
-//        }
-//    };
-//
-//    private void miUbicacion() {
-//
-//        if (ActivityCompat.checkSelfPermission(com.example.prog2.microdigit.Fragments.MapFragment.super.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-//                && ActivityCompat.checkSelfPermission(com.example.prog2.microdigit.Fragments.MapFragment.super.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//
-//            return;
-//        }
-//        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-//        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//        actualizarUbicacion(location);
-//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0,locListener);
-//
-//    }
-
-    //////////***********///////
-
 
 }
